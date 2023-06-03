595f1b81189f1c41f4d8d7093edb8e3d8e29c7ba
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-06-04 00:17:11 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientEls = document.querySelector("#ingredients");
+const listEl = ingredients.map((ingredient) => {
+  const ilEl = document.createElement("li");
+  ilEl.textContent = ingredient;
+  ilEl.classList.add("item");
+  return ilEl;
+});
+
+console.log(listEl);
+ingredientEls.append(...listEl);
