b822e14e5c003ad8a64668278c29e772d39505c8
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-06-05 15:00:37 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -4,14 +4,35 @@ const decrementBtnEL = document.querySelector('[data-action="decrement"]');
 const incrementBtnEl = document.querySelector('[data-action="increment"]');
 
 counterValue.textContent = 0;
-function handelDecrementClick() {
-  return (counterValue.textContent -= 1);
-}
-console.log(handelDecrementClick);
+let NumberValue = Number(counterValue.textContent);
 function handelIncrementClick() {
-  return counterValue.textContent++;
+  NumberValue += 1;
+  counterValue.textContent = NumberValue;
 }
-console.log(handelIncrementClick);
 
+function handelDecrementClick() {
+  NumberValue -= 1;
+  counterValue.textContent = NumberValue;
+}
 decrementBtnEL.addEventListener("click", handelDecrementClick);
 incrementBtnEl.addEventListener("click", handelIncrementClick);
+
+// варінт, якщо міняються будь-які змінні
+// counterValue.textContent = 0;
+// decrementBtnEL.textContent = -2;
+// incrementBtnEl.textContent = 2;
+// let NumberValue = Number(counterValue.textContent);
+// let NumberDecrementBtnEL = Number(decrementBtnEL.textContent);
+// let NumberIncrementBtnEl = Number(incrementBtnEl.textContent);
+
+// function handelIncrementClick() {
+//   NumberValue += NumberIncrementBtnEl;
+//   counterValue.textContent = NumberValue;
+// }
+// function handelDecrementClick() {
+//   NumberValue += NumberDecrementBtnEL;
+//   counterValue.textContent = NumberValue;
+// }
+
+// decrementBtnEL.addEventListener("click", handelDecrementClick);
+// incrementBtnEl.addEventListener("click", handelIncrementClick);
