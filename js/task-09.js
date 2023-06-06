31780ae83e155964124c131658822b0127780846
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-06-06 22:45:46 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const refs = {
+  backgroundColorBody: document.querySelector(".widget"),
+  btnChangeColor: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  colorEl: document.querySelector(".color"),
+};
+
+refs.btnChangeColor.addEventListener("click", handelButtonClick);
+
+function handelButtonClick() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorEl.textContent = refs.body.style.backgroundColor;
+  console.log("click");
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
